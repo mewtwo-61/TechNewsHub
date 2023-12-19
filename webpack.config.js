822d3bc7fc6728bbca1ced3d7b9dc78a297a7c8d
Pegah Chendari <chendaripegah@gmail.com>
2023-12-18 17:09:11 -0800

    “update

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',  
+      template: path.resolve(__dirname, './public/index.html'),
+    }),
+  ],
+  devServer: {
+    static: {
+        directory: path.join(__dirname, 'public'),
+      },
+      host: 'localhost',
+      port: 8080,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      open: true,
+      hot: true,
+      liveReload: true,
+      proxy: {
+        '/api/**': {
+          target: 'http://localhost:3001/',
+          secure: false,
+        },
+      },
+  }
+};
